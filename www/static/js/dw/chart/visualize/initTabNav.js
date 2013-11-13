004bb2612222a5e98e86df25a6059b88ceef6b4c
Gregor Aisch <contact@vis4.net>
2013-11-13 16:34:20 +0100

    axes editor nav

--- www/static/js/dw/chart/visualize/initTabNav.js
+++ www/static/js/dw/chart/visualize/initTabNav.js
@@ -1,9 +1,16 @@
 
-define(function() {
+define(['./axesEditor'], function(axesEditor) {
 
     return function() {
 
+        $('.visualize-left .axes-preview .btn-customize').click(function(evt) {
+            evt.preventDefault();
+            $('.visualize-left').removeClass('span2-5').addClass('span5');
+            $('.visualize-right').hide();
+            $('.visualize-left .chart-type-axis-preview').addClass('hide');
+            $('.visualize-left .axes-editor').removeClass('hide');
 
+            axesEditor.initUI();
         });
 
     };
