953953f3cd7632ce635ec66954fe6b23b776dcc2
Gregor Aisch <contact@vis4.net>
2013-11-13 16:35:06 +0100

    re-implemented axes editor

--- www/static/js/dw/chart/visualize/axesEditor.js
+++ www/static/js/dw/chart/visualize/axesEditor.js
@@ -1,8 +1,180 @@
 
-define(['selectize', 'drag_drop', 'sortable'], function() {
+define(['dragdrop'], function() {
+
+    var _chart, _visJSON;
+
+    jQuery.fn.outerHTML = function(s) {
+        return s
+            ? this.before(s).remove()
+            : jQuery("<p>").append(this.eq(0).clone()).html();
+    };
 
     return {
         init: function(chart, visJSON) {
+            _chart = chart;
+            _visJSON = visJSON;
+        },
+
+        initUI: function() {
+            var ds = chart.dataset(),
+                colDiv = $('.axes-editor .columns'),
+                axesDiv = $('.axes-editor .axes'),
+                axisTpl = $('.template-axis').html(),
+                visAxes = dw.backend.currentVis.axes(true),
+                axesCfg = dw.backend.currentVis.axes(),
+                _storeTo;
+
+            $('.column', colDiv).remove(); // remove existing columns
+
+            ds.eachColumn(initColumn);
+
+            _.each(_visJSON.axes, initAxis);
+
+            initDragDrop();
+
+            function initColumn(column) {
+                var c = $('<div />')
+                            .html(column.title())
+                            .attr('data-id', column.name())
+                            .addClass('column')
+                            .appendTo(colDiv);
+            }
+
+            function initAxis(axis, id) {
+                var d = $('<div />').html(axisTpl)
+                            .addClass('axis')
+                            .data('id', id)
+                            .addClass(axis.multiple ? 'multiple' : 'single')
+                            .appendTo(axesDiv);
+                $('.axis-title', d).html(axis.id);
+
+                // populate axes
+                var columns = visAxes[id],
+                    axesColDiv = $('.axis-columns', d);
+                if (!_.isArray(columns)) columns = [columns];
+
+                _.each(columns, function(col) {
+                    $('<div />').addClass('column')
+                        .html(col.title())
+                        .attr('data-id', col.name())
+                        .appendTo(axesColDiv);
+                });
+                axesColDiv.sortable({ revert: true });
+            }
+
+            function initDragDrop() {
+
+                $('.column', colDiv).draggable({
+                    connectToSortable: '.axis-columns',
+                    helper: 'clone',
+                    revertDuration: 100,
+                    zIndex: 10,
+                    appendTo: ".axes-editor",
+                    start: function(evt, ui) {
+                        ui.helper.css({ 'margin-top': -$(window).scrollTop() });
+                    },
+                    stop: function(evt, ui) {
+                        ui.helper.css({ 'margin-top': 0 });
+                    }
+                });
+
+                $(".axis-columns").sortable({
+                    connectWith: '.axis-columns',
+                    revert: true,
+                    scroll: true,
+                    revertDuration: 100,
+                    placeholder: "sortable-placeholder"
+                })
+
+                .droppable({
+                    activeClass: 'highlight',
+                    revertDuration: 100,
+
+                    over: function(evt, ui) {
+                        // check if we can accept the draggable
+                        var context = this,
+                            $axisColumns = $(this),
+                            $axis = $axisColumns.parents('.axis'),
+                            draggedColumn = ui.draggable.data('id'),
+                            accept = true;
+
+                        if (_visJSON.axes[$axis.data('id')].accepts.indexOf(ds.column(draggedColumn).type()) < 0) {
+                            ui.draggable.addClass('not-accepted');
+                            ui.helper.addClass('not-accepted');
+                        }
+
+                        $('.column', $axisColumns).each(function() {
+                            if (toBeRemoved(this, ui, context)) {
+                                $(this).addClass('soon-to-be-removed');
+                            }
+                        });
+                    },
+
+                    out: function(evt, ui) {
+                        $('.column', this).removeClass('soon-to-be-removed');
+                    },
+
+                    drop: function(event, ui) {
+                        var context = this,
+                            $axisColumns = $(this),
+                            $axis = $axisColumns.parents('.axis'),
+                            draggedColumn = ui.draggable.data('id'),
+                            newColumn = ui.draggable.hasClass('ui-draggable');
+
+                        ui.draggable.removeClass('soon-to-be-removed');
+                        if (ui.draggable.hasClass('not-accepted')) {
+                            ui.draggable.remove();
+                        }
+
+                        $('.column.soon-to-be-removed', $axisColumns).remove();
+                        clearTimeout(_storeTo);
+                        _storeTo = setTimeout(storeConfig, 500);
+                    },
+
+                });
+
+                /*
+                 * @el one of the existing columns
+                 */
+                function toBeRemoved(el, ui, context) {
+                    var axis = $(context).parents('.axis').data('id'),
+                        isSingle = _visJSON.axes[axis].multiple !== true,
+                        dragged = ui.draggable.data('id');
+
+                    if (ui.helper.hasClass('not-accepted')) return false;
+
+                    // check if this is a single axis
+                    if (isSingle) {
+                        return true;
+                    } else {
+                        if (axesCfg[axis].indexOf(dragged) >= 0) {
+                            return $(el).data('id') == dragged;
+                        }
+                    }
+                    return false;
+                }
+
+                $('.column').disableSelection();
+            }
+
+            function storeConfig() {
+                $('.axes-editor .axis').each(function() {
+                    var axis = $(this);
+                    if (axis.hasClass('single')) {
+                        axesCfg[axis.data('id')] = $('.column', axis).data('id');
+                    } else {
+                        axesCfg[axis.data('id')] = [];
+                        $('.column', axis).each(function() {
+                            axesCfg[axis.data('id')].push($(this).data('id'));
+                        });
+                    }
+                });
+                console.log(axesCfg);
+            }
+        },
+
+        load: function() {
+
             var ds = chart.dataset(),
                 $c = $('.axes-editor'),
                 columnUsed = {},
